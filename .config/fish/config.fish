643816d57756637fbcb7c83249f4cf83493c58eb
Salix0v0 <104568454+Salix0v0@users.noreply.github.com>
2026-04-12 23:05:20 +0800

    fish vi模式,install.sh,fish 快捷键设置

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,7 +28,40 @@ end
 # <<< conda initialize <<<
 
 # uv
+fish_vi_key_bindings
+# === 手动补全 Visual 模式下的缺失键位 ===
+
+# 让可视模式下的 gg 移动到光标/命令行的最前端
+bind -M visual gg beginning-of-buffer
+
+# 让可视模式下的 G 移动到光标/命令行的最后端
+bind -M visual G end-of-buffer
+# 绑定 y 复制到系统剪贴板（visual 模式）
+bind -M visual y 'fish_clipboard_copy; commandline -f end-selection repaint-mode'
+
+# 绑定 p 从系统剪贴板粘贴（normal 模式）
+bind -M normal p fish_clipboard_paste
+bind -M visual p fish_clipboard_paste
+# 补充：如果你发现在可视模式下按 0 (行首) 和 $ (行尾) 也不管用，可以一并加上：
+bind -M visual 0 beginning-of-line
+bind -M visual '$' end-of-line
+function _ghostty_open_scrollback
+    sleep 0.1
+    set path (commandline)
+    commandline "nvim $path"
+    commandline -f execute
+end
+
+bind --mode insert ctrl-shift-h _ghostty_open_scrollback
+set -g theme_display_vi no
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias lazydot='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 fish_add_path "/home/salix/.local/bin"
 fish_add_path "/home/salix/.local/share/npm/bin"
+# 有已存在的 session 就 attach，没有就新建
+# 自动进入 tmux（防嵌套 + 防非交互式 shell）
+# if status is-interactive
+#     and command -q tmux
+#     and not set -q TMUX
+#     tmux new-session
+# end
