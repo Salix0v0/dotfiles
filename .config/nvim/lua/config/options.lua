08b494edb4441301630228fe53e85d2f2ef8086a
Salix0v0 <104568454+Salix0v0@users.noreply.github.com>
2026-07-03 13:40:57 +0800

    osc52

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,25 +12,26 @@
 -- 其他选项: "none" | "rounded" | "double" | "solid" | "shadow"
 -- ~/.config/nvim/lua/config/options.lua
 
-vim.opt.scrolloff = 0     -- 上下保留行数（默认8），改小
+vim.opt.scrolloff = 0 -- 上下保留行数（默认8），改小
 vim.opt.sidescrolloff = 0 -- 左右保留列数
 vim.opt.spelllang = { "en_us", "cjk" }
 -- vim.opt.spell = false
 vim.g.suda_smart_edit = 1
 vim.o.winborder = "rounded"
 -- 可选: "single" | "double" | "rounded" | "solid"
--- -- 强制使用系统剪贴板
--- vim.opt.clipboard = "unnamedplus"
---
--- -- 使用 Neovim 0.10+ 原生的 OSC 52 剪贴板提供程序
--- vim.g.clipboard = {
---   name = "OSC 52",
---   copy = {
---     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
---     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
---   },
---   paste = {
---     ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
---     ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
---   },
--- }
+
+-- 强制使用系统剪贴板
+vim.opt.clipboard = "unnamedplus"
+
+-- 使用 Neovim 0.10+ 原生的 OSC 52 剪贴板提供程序
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
