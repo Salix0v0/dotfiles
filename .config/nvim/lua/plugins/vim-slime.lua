f4409686e445c3527eb4b437b21f1425cf943507
Salix0v0 <104568454+Salix0v0@users.noreply.github.com>
2026-07-07 01:24:14 +0000

    add vim-slime

---
+++ .config/nvim/lua/plugins/vim-slime.lua
@@ -0,0 +1,18 @@
+return {
+  "jpalardy/vim-slime",
+  init = function()
+    vim.g.slime_target = "tmux"
+
+    vim.g.slime_default_config = {
+      socket_name = "default",
+    }
+
+    vim.g.slime_dont_ask_default = 1
+
+    -- 关键：让 tmux 用 bracketed paste 方式粘贴
+    vim.g.slime_bracketed_paste = 1
+
+    -- 如果右侧是 IPython，也加上
+    vim.g.slime_python_ipython = 1
+  end,
+}
